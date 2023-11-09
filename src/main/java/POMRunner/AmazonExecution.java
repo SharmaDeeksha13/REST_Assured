82f7047613865c609209dd9889827f11a6cc31e3
deeksha.sharma <deeksha13aug@gmail.com>
2023-11-08 23:57:09 -0500

    commit

--- src/main/java/POMRunner/AmazonExecution.java
+++ src/main/java/POMRunner/AmazonExecution.java
@@ -41,6 +41,29 @@ public class AmazonExecution {
 
     }
 
+    @Test
+    public void launch1() throws InterruptedException, IOException {
+        System.setProperty("webdriver,chrome.driver","D://Softwares//chromedriver_win32//chromedriver.exe");
+//        ChromeOptions c1= new ChromeOptions();
+//        c1.addArguments("--headless");
+//        driver = new ChromeDriver(c1);
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        page1 p1  = PageFactory.initElements(driver,page1.class);
+        p1.clickOnGrocery();
+        Thread.sleep(3000);
+        driver.navigate().back();
+
+
+        TakesScreenshot tsc= (TakesScreenshot)driver;
+        File source= tsc.getScreenshotAs(OutputType.FILE);
+        File newDestination= new File("C:\\Users\\deeks\\Downloads\\new1.jpg");
+        FileUtils.copyFile(source,newDestination);
+        Thread.sleep(3000);
+
+    }
+
     @AfterClass
     public void close()
     {
